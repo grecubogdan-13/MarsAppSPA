baea08c7af215a39ccf3352de0b63a71ac12fade
Grecu Bogdan <BogdanOctavian.Grecu@softwire.com>
2022-06-30 16:10:18 +0100

    task 1 and 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
+import nasaLogo from './NASA.svg';
 import './App.css';
+
+function MyTemplate(props: any){
+  return <div>
+    <h1>{props.name}</h1>
+    <p>{props.paragraph1}</p>
+    <p>{props.paragraph2}</p>
+    <img src={props.image} className="App-logo" alt="logo" />
+  </div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <MyTemplate name="NASA" paragraph1="agsgahuoaoa aung aa iabng ka " paragraph2="aggraathh shsh siklesa lsjh ls" image={nasaLogo} />
+        </header>
+      </div>
   );
 }
 
